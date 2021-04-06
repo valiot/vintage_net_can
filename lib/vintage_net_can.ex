fd476260be0f227057c0a2ee7b5fc84c981f92a4
alde103 <aldebaran_103@hotmail.com>
2021-04-06 01:15:15 -0500

    VintageNet.Technology behavior & docs added

--- lib/vintage_net_can.ex
+++ lib/vintage_net_can.ex
@@ -1,18 +1,89 @@
 defmodule VintageNetCan do
+  @behaviour VintageNet.Technology
+
+  alias VintageNet.Interface.RawConfig
+  alias VintageNetCan.{CanConfig, SocketCanConfig}
+
   @moduledoc """
-  Documentation for `VintageNetCan`.
+  Support for common CAN interface configurations
+  Configurations for this technology are maps with a `:type` field set to
+  `VintageNetCan`. The following additional fields are supported:
+  * `:can` - IP command options for CAN. See VintageNet.CanConfig.
+  * `:socket` - SocketCan daemon options (Optional). See VintageNet.SocketCanConfig.
+
+  An example with only CAN configuration is:
+  ```elixir
+  %{type: VintageNetCan, can: %{bitrate: 500000, loopback: "on"}}
+  ```
+  
+  To see all available option, execute the following shell command:
+
+  ```bash
+  ip link set can0 type can -h
+  ```
+
+  An example with CAN and SocketCand configuration is:
+  ```elixir
+  %{
+    type: VintageNetCan,
+    can: %{bitrate: 500000, loopback: "on"},
+    socket: %{
+      port: 29536,
+      interfaces: ["can0"],
+      linked_interface: "lo",
+    }
+  }
+  ```
   """
 
-  @doc """
-  Hello world.
+  @impl VintageNet.Technology
+  def normalize(%{type: __MODULE__} = config) do
+    config
+    |> CanConfig.normalize()
+    |> SocketCanConfig.normalize()
+  end
 
-  ## Examples
+  @impl VintageNet.Technology
+  def to_raw_config(
+        ifname,
+        %{type: __MODULE__, socket: %{interfaces: interfaces, linked_interface: linked_interface}} =
+          config,
+        opts
+      ) do
+    normalized_config = normalize(config)
 
-      iex> VintageNetCan.hello()
-      :world
+    %RawConfig{
+      ifname: ifname,
+      type: __MODULE__,
+      source_config: normalized_config,
+      required_ifnames: Enum.unique([ifname, linked_interface] ++ interfaces)
+    }
+    |> CanConfig.add_config(normalized_config, opts)
+    |> SocketCanConfig.add_config(normalized_config, opts)
+  end
 
-  """
-  def hello do
-    :world
+  @impl VintageNet.Technology
+  def to_raw_config(ifname, %{type: __MODULE__} = config, opts) do
+    normalized_config = normalize(config)
+
+    %RawConfig{
+      ifname: ifname,
+      type: __MODULE__,
+      source_config: normalized_config,
+      required_ifnames: [ifname]
+    }
+    |> CanConfig.add_config(normalized_config, opts)
+    |> SocketCanConfig.add_config(normalized_config, opts)
+  end
+
+  @impl VintageNet.Technology
+  def ioctl(_ifname, _command, _args) do
+    {:error, :unsupported}
+  end
+
+  @impl VintageNet.Technology
+  def check_system(_opts) do
+    # TODO
+    :ok
   end
 end
