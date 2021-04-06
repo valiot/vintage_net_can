c206ff1e6e16587d26c38ef1d17c0dc18a8762ae
alde103 <aldebaran_103@hotmail.com>
2021-04-05 21:16:13 -0500

    initial commit

---
+++ lib/vintage_net_can.ex
@@ -0,0 +1,18 @@
+defmodule VintageNetCan do
+  @moduledoc """
+  Documentation for `VintageNetCan`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> VintageNetCan.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/vintage_net_can_test.exs
@@ -0,0 +1,8 @@
+defmodule VintageNetCanTest do
+  use ExUnit.Case
+  doctest VintageNetCan
+
+  test "greets the world" do
+    assert VintageNetCan.hello() == :world
+  end
+end
